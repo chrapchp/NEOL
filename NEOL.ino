67bf509d2a1adcc87288964632b81d5ff84f91b9
unknown <=chrapchp@flashsense.com>
2017-10-26 12:33:40 -0700

    base

---
+++ NEOL.ino
@@ -0,0 +1,338 @@
+/**
+*  @file    NutrientEOL.ino
+*  @author  peter c
+*  @date    10/25/2017
+*  @version 0.1
+*
+*
+*  @section DESCRIPTION
+*  NEOL Arduino Nano
+** @section HISTORY
+** 2017Oct25 - created
+*/
+#include <HardwareSerial.h>
+#include <TimeAlarms.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h> // for RTC
+#include <Streaming.h>
+
+#include <DA_Analoginput.h>
+#include <DA_Discreteinput.h>
+#include <DA_DiscreteOutput.h>
+#include <DA_DiscreteOutputTmr.h>
+#include <DA_HOASwitch.h>
+
+#include "PlantModbus.h"
+
+
+
+
+// comment out to not include terminal processing
+#define PROCESS_TERMINAL
+// comment out to disable modbus
+#define PROCESS_MODBUS
+// refresh intervals
+#define POLL_CYCLE_SECONDS 2 // sonar and 1-wire refresh rate
+
+#define ALARM_REFRESH_INTERVAL 100 // ms
+// One Wire - Hydroponic temperatures
+// 
+#define TEMPERATURE1 8 // pin
+#define TEMPERATURE2 7 // pin
+#define TEMPERATURE3 14 // pin
+#define TEMPERATURE4 4 // pin
+
+#define ONE_TEMPERATURE_PRECISION 9
+
+OneWire oneWireBus1(TEMPERATURE1);
+OneWire oneWireBus2(TEMPERATURE2);
+OneWire oneWireBus3(TEMPERATURE3);
+OneWire oneWireBus4(TEMPERATURE4);
+DallasTemperature T1_TT004(& oneWireBus1);
+DallasTemperature N1_TT004(& oneWireBus2);
+DallasTemperature N2_TT004(& oneWireBus3);
+DallasTemperature N3_TT004(& oneWireBus4);
+
+
+DA_DiscreteOutput T1_XY004 = DA_DiscreteOutput(3, LOW); // V1
+DA_DiscreteOutput N1_XY004 = DA_DiscreteOutput(11, LOW); // V2
+DA_DiscreteOutput N2_XY004 = DA_DiscreteOutput(10, LOW); // V3
+DA_DiscreteOutput N3_XY004 = DA_DiscreteOutput(9, LOW); // V4
+
+DA_AnalogInput T1_PT004 = DA_AnalogInput(A1, 0.0, 5.); // min max
+DA_AnalogInput N1_PT004 = DA_AnalogInput(A2, 0.0, 5.); // min max
+DA_AnalogInput N2_PT004 = DA_AnalogInput(A6, 0.0, 5.); // min max
+DA_AnalogInput N3_PT004 = DA_AnalogInput(A7, 0.0, 5.); // min max
+
+
+// HEARTBEAT
+
+unsigned int heartBeat = 0;
+
+struct _AlarmEntry
+{
+  time_t epoch;
+  AlarmId id = dtINVALID_ALARM_ID;
+  bool firstTime = true;
+};
+
+typedef _AlarmEntry AlarmEntry;
+AlarmEntry onRefreshAnalogs; // sonar and 1-wire read refresh
+AlarmEntry onFlowCalc; // flow calculations
+HardwareSerial *tracePort = & Serial2;
+void onEdgeDetect(bool state, int pin)
+{
+  #ifdef PROCESS_TERMINAL
+  *tracePort << "Edge Detection:" << state << " on pin " << pin << endl;
+  #endif
+}
+
+void printOneWireAddress(HardwareSerial *tracePort, DeviceAddress aDeviceAddress, bool aCR)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    // zero pad the address if necessary
+    if (aDeviceAddress[i] < 16)
+      *tracePort << '0';
+    tracePort -> print(aDeviceAddress[i], HEX);
+  }
+  if (aCR)
+    *tracePort << endl;
+}
+
+void init1WireTemperatureSensor(DallasTemperature * sensor, int idx)
+{
+  DeviceAddress address;
+  sensor -> begin();
+  if (sensor -> getAddress(address, 0))
+  {
+
+#ifdef PROCESS_TERMINAL
+    *tracePort << "Channel " << idx << " 1Wire Temperature initialized. Address =  ";
+    printOneWireAddress(tracePort, address, true);
+#endif
+
+    sensor -> setResolution(address, ONE_TEMPERATURE_PRECISION);
+  }
+  else
+  {
+
+#ifdef PROCESS_TERMINAL
+    *tracePort << "Unable to find address for 1Wire Temperature Device @ " << idx << endl;
+#endif
+
+  }
+}
+
+void initHydroponicOneWireTemps()
+{
+  init1WireTemperatureSensor(& T1_TT004, 1);
+  init1WireTemperatureSensor(& N1_TT004, 2);
+  init1WireTemperatureSensor(& N2_TT004, 3);
+  init1WireTemperatureSensor(& N3_TT004, 4);
+}
+
+int polling; // 1=polling analogs, 2=polling digitals, -1 nothing
+int currentTogglePin; // -1 none, >0 pin
+void setup()
+{
+
+#ifdef PROCESS_TERMINAL
+  tracePort -> begin(9600);
+
+#endif
+
+#ifdef PROCESS_MODBUS
+  slave.begin(19200);
+#endif
+
+  randomSeed(analogRead(3));
+  onRefreshAnalogs.id = Alarm.timerRepeat(POLL_CYCLE_SECONDS, doOnPoll);
+  initHydroponicOneWireTemps();
+}
+
+void loop()
+{
+
+#ifdef PROCESS_MODBUS
+ // refreshModbusRegisters();
+ // slave.poll(modbusRegisters, MODBUS_REG_COUNT);
+ // processModbusCommands();
+#endif
+
+  Alarm.delay(ALARM_REFRESH_INTERVAL);
+}
+
+// update sonar and 1-wire DHT-22 readings
+void doOnPoll()
+{
+  doReadAnalogs();
+  doProcess1WireTemperatures();
+  heartBeat++;
+
+    refreshModbusRegisters();
+  slave.poll(modbusRegisters, MODBUS_REG_COUNT);
+  processModbusCommands();
+}
+
+
+
+void doPoll1WireTemperature( DallasTemperature *sensor, int idx ) 
+{
+  sensor->requestTemperatures();
+  #ifdef PROCESS_TERMINAL
+    *tracePort << "Temperature " << idx << " = " << sensor->getTempCByIndex(0) << " C" << endl;
+  #endif
+}
+
+
+void doProcess1WireTemperatures()
+{
+
+  doPoll1WireTemperature( &T1_TT004, 0);
+  doPoll1WireTemperature( &N1_TT004, 1);
+  doPoll1WireTemperature( &N2_TT004, 2);
+  doPoll1WireTemperature( &N3_TT004, 3); 
+}
+
+void doReadAnalogs()
+{
+  T1_PT004.refresh();
+  N1_PT004.refresh();
+  N2_PT004.refresh();
+
+  #ifdef PROCESS_TERMINAL
+      T1_PT004.serialize( tracePort, true);
+      N1_PT004.serialize( tracePort, true);
+      N2_PT004.serialize( tracePort, true);
+      N3_PT004.serialize( tracePort, true);
+  #endif
+
+}
+
+void doUpdateOutputs()
+{
+}
+
+// 
+/*
+** Modbus related functions
+*/
+
+#ifdef PROCESS_MODBUS
+void refreshModbusRegisters()
+{
+  /*
+  #define HR_TEMPERATURE1 HOLDING_REGISTER_READ_OFFSET
+  #define HR_TEMPERATURE2 HR_TEMPERATURE1 + 1
+  #define HR_TEMPERATURE3 HR_TEMPERATURE2 + 1
+  #define HR_TEMPERATURE4 HR_TEMPERATURE3 + 1
+  #define HR_PRESSURE1 HR_TEMPERATURE4 + 1
+  #define HR_PRESSURE2 HR_PRESSURE1 + 1
+  #define HR_PRESSURE3 HR_PRESSURE2 + 1
+  #define HR_PRESSURE4 HR_PRESSURE3 + 1
+*/
+  blconvert.val = T1_TT004.getTempCByIndex(0) * 100;
+  modbusRegisters[HR_TEMPERATURE1] = blconvert.regsl[0];
+  modbusRegisters[HR_TEMPERATURE1 + 1] = blconvert.regsl[1];
+
+  blconvert.val = N1_TT004.getTempCByIndex(0) * 100;
+  modbusRegisters[HR_TEMPERATURE2] = blconvert.regsl[0];
+  modbusRegisters[HR_TEMPERATURE2 + 1] = blconvert.regsl[1];
+
+  blconvert.val = N2_TT004.getTempCByIndex(0) * 100;
+  modbusRegisters[HR_TEMPERATURE3] = blconvert.regsl[0];
+  modbusRegisters[HR_TEMPERATURE3 + 1] = blconvert.regsl[1];
+
+  blconvert.val = N3_TT004.getTempCByIndex(0) * 100;
+  modbusRegisters[HR_TEMPERATURE4] = blconvert.regsl[0];
+  modbusRegisters[HR_TEMPERATURE4 + 1] = blconvert.regsl[1];
+
+  blconvert.val = T1_PT004.getScaledSample() * 100;
+  modbusRegisters[HR_PRESSURE1] = blconvert.regsl[0];
+  modbusRegisters[HR_PRESSURE1 + 1] = blconvert.regsl[1];
+
+  blconvert.val = N1_PT004.getScaledSample() * 100;
+  modbusRegisters[HR_PRESSURE2] = blconvert.regsl[0];
+  modbusRegisters[HR_PRESSURE2 + 1] = blconvert.regsl[1];
+
+  blconvert.val = N2_PT004.getScaledSample() * 100;
+  modbusRegisters[HR_PRESSURE3] = blconvert.regsl[0];
+  modbusRegisters[HR_PRESSURE3 + 1] = blconvert.regsl[1];
+
+  blconvert.val = heartBeat;
+  modbusRegisters[HR_PRESSURE4] = blconvert.regsl[0];
+  modbusRegisters[HR_PRESSURE4 + 1] = blconvert.regsl[1];
+
+  
+
+  // modbusRegisters[HR_LED_DUTY_CYCLE] = plantStrip.getDutyCycle();
+  // modbusRegisters[HR_LED_DUTY_CYCLE_PERIOD] = dutyCycleChangeAlarm.epoch;
+  /*
+  blconvert.val = lightsOn.epoch;
+  modbusRegisters[HR_LED_ON_TIME] = blconvert.regsl[0];
+  modbusRegisters[HR_LED_ON_TIME + 1] = blconvert.regsl[1];
+  blconvert.val = lightsOff.epoch;
+  modbusRegisters[HR_LED_OFF_TIME] = blconvert.regsl[0];
+  modbusRegisters[HR_LED_OFF_TIME + 1] = blconvert.regsl[1];
+  blconvert.val = now();
+  modbusRegisters[HR_CURRENT_TIME] = blconvert.regsl[0];
+  modbusRegisters[HR_CURRENT_TIME + 1] = blconvert.regsl[1];
+  */
+  // modbusRegisters[ CS_LED_STATUS ] = plantStrip.isLightsOn();
+  // modbusRegisters[ CS_LED_STATUS ] = plantStrip.isLightsOn();
+  // writeModbusCoil( COIL_STATUS_READ_WRITE_OFFSET, CS_LED_STATUS, plantStrip.isLightsOn());
+  // modbusRegisters[HR_AMBIENT_TEMPERATURE] = (int) TE_001.getScaledSample() * 10;
+  // modbusRegisters[HR_SOIL_MOISTURE] = (int) QE_001.getScaledSample() * 10;
+  // bitWrite(modbusRegisters[ COIL_STATUS_READ_OFFSET], CS_SET_LED_ON, plantStrip.isLightsOn());
+}
+
+
+bool getModbusCoilValue( unsigned short startAddress, unsigned short bitPos)
+{
+
+  *tracePort << "reading at " << startAddress << " bit offset " << bitPos << "value=" << bitRead(modbusRegisters[startAddress + (int)(bitPos / 16)], bitPos % 16 ) << endl;
+
+  return ( bitRead(modbusRegisters[startAddress + (int)(bitPos / 16)], bitPos % 16 ) );
+}
+
+
+void checkAndActivateDO( unsigned int bitOffset, DA_DiscreteOutput *aDO )
+{
+   if(  getModbusCoilValue( COIL_STATUS_READ_WRITE_OFFSET, bitOffset )  )
+      aDO->activate();
+}
+
+void checkAndResetDO( unsigned int bitOffset, DA_DiscreteOutput *aDO )
+{
+   if(  getModbusCoilValue( COIL_STATUS_READ_WRITE_OFFSET, bitOffset )  )
+      aDO->reset();
+}
+
+
+
+
+void processValveCommands()
+{
+  checkAndActivateDO( VALVE1_OPEN, &T1_XY004 );
+  checkAndActivateDO( VALVE1_CLOSE, &T1_XY004 );  
+
+  checkAndActivateDO( VALVE2_OPEN, &N1_XY004 );
+  checkAndActivateDO( VALVE2_CLOSE, &N1_XY004 ); 
+
+  checkAndActivateDO( VALVE3_OPEN, &N2_XY004 );
+  checkAndActivateDO( VALVE3_CLOSE, &N2_XY004 ); 
+  
+  checkAndActivateDO( VALVE4_OPEN, &N3_XY004 );
+  checkAndActivateDO( VALVE4_CLOSE, &N3_XY004 ); 
+ 
+}
+
+void processModbusCommands()
+{
+  processValveCommands();  
+}
+
+#endif
+
---
+++ plantModbus.h
@@ -0,0 +1,106 @@
+/**
+ * @file 	plantModbus.h
+ * @version     0.1
+ * @date        2017May3
+ * @author 	pjc
+
+ *
+ * @description
+ *  Helpers for plant lighting and control using Modbus
+ *
+ * Using arduino modbus implementation @
+ * https://github.com/smarmengol/Modbus-Master-Slave-for-Arduino
+*/
+
+
+#include <SoftwareSerial.h>
+#include <ModbusRtu.h>
+
+ 
+#define COIL_STATUS_READ_WRITE_OFFSET 0
+#define COIL_STATUS_WRITE_START_BIT	(16*5)  // 80 coils/bits for reads then followed by write memory for coils
+
+#define HOLDING_REGISTER_READ_OFFSET 10		// start read holding regisers
+#define HOLDING_REGISTER_WRITE_OFFSET 30
+
+
+#define HR_TEMPERATURE1    	HOLDING_REGISTER_READ_OFFSET
+#define HR_TEMPERATURE2  	HR_TEMPERATURE1 + 2
+#define HR_TEMPERATURE3    	HR_TEMPERATURE2 + 2
+#define HR_TEMPERATURE4  	HR_TEMPERATURE3 + 2
+#define HR_PRESSURE1		HR_TEMPERATURE4 + 2
+#define HR_PRESSURE2		HR_PRESSURE1    + 2
+#define HR_PRESSURE3		HR_PRESSURE2    + 2
+#define HR_PRESSURE4		HR_PRESSURE3    + 2
+
+//#define HR_HEARTBEAT 
+
+#define MODBUS_REG_COUNT HOLDING_REGISTER_WRITE_OFFSET + HR_PRESSURE4 + 1
+// coil write commands
+#define VALVE1_OPEN			COIL_STATUS_WRITE_START_BIT //bit position
+#define VALVE1_CLOSE		VALVE1_OPEN 	+ 1		
+#define VALVE2_OPEN			VALVE1_CLOSE 	+ 1  
+#define VALVE2_CLOSE		VALVE2_OPEN 	+ 1  
+#define VALVE3_OPEN			VALVE2_CLOSE 	+ 1  
+#define VALVE3_CLOSE		VALVE3_OPEN     + 1
+#define VALVE4_OPEN			VALVE2_CLOSE 	+ 1  
+#define VALVE4_CLOSE		VALVE4_OPEN     + 1 
+
+
+
+//#define CS_RESET_TO_DEFAULTS	 CS_SET_LED_OFF + 1
+
+// coil statuses
+//#define CS_LED_STATUS			COIL_STATUS_READ_WRITE_OFFSET
+/*
+
+#define HR_AMBIENT_TEMPERATURE HOLDING_REGISTER_READ_OFFSET
+#define HR_SOIL_MOISTURE	   HR_AMBIENT_TEMPERATURE + 1
+#define HR_LED_DUTY_CYCLE	   HR_SOIL_MOISTURE + 1
+#define HR_LED_DUTY_CYCLE_PERIOD HR_LED_DUTY_CYCLE + 1
+#define HR_CURRENT_TIME			HR_LED_DUTY_CYCLE_PERIOD + 1   // UTC
+#define HR_LED_ON_TIME			HR_CURRENT_TIME + 2
+#define HR_LED_OFF_TIME			HR_LED_ON_TIME + 2
+
+#define HR_SET_TIME				HOLDING_REGISTER_WRITE_OFFSET  // UTC
+#define HR_SET_LED_ON_TIME		HR_SET_TIME + 2
+#define HR_SET_LED_OFF_TIME		HR_SET_LED_ON_TIME + 2
+#define HR_SET_DUTY_CYCLE		HR_SET_LED_OFF_TIME + 2
+#define HR_SET_DUTY_CYCLE_PERIOD HR_SET_DUTY_CYCLE + 1
+
+#define MODBUS_REG_COUNT HOLDING_REGISTER_WRITE_OFFSET + HR_SET_DUTY_CYCLE_PERIOD + 1
+
+// coil write commands
+#define CS_SET_LED_ON			 COIL_STATUS_WRITE_START_BIT //bit position
+#define CS_SET_LED_OFF			 CS_SET_LED_ON + 1
+#define CS_RESET_TO_DEFAULTS	 CS_SET_LED_OFF + 1
+
+// coil statuses
+#define CS_LED_STATUS			COIL_STATUS_READ_WRITE_OFFSET
+
+*/
+#define MB_SLAVE_ID				1
+#define MB_SERIAL_PORT			0
+
+
+
+union
+{
+  int regsf[2];
+  float val;
+} 
+bfconvert;
+
+
+union
+{
+  int regsl[2];
+  long val;
+} 
+blconvert;
+
+uint16_t modbusRegisters[MODBUS_REG_COUNT];
+
+Modbus slave(MB_SLAVE_ID, MB_SERIAL_PORT); 
+
+
