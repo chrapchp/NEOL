577223039f3f3b7a458dbc9cd4180219d2d7aad9
chrapchp <chrapchp@flashsense.com>
2017-10-29 13:39:53 -0600

    updated as per official tagnames, seperated modbus address accross units and specific to units

--- NEOL.ino
+++ NEOL.ino
@@ -44,20 +44,20 @@ OneWire oneWireBus1(TEMPERATURE1);
 OneWire oneWireBus2(TEMPERATURE2);
 OneWire oneWireBus3(TEMPERATURE3);
 OneWire oneWireBus4(TEMPERATURE4);
-DallasTemperature T1_TT004(& oneWireBus1);
-DallasTemperature N1_TT004(& oneWireBus2);
-DallasTemperature N2_TT004(& oneWireBus3);
-DallasTemperature N3_TT004(& oneWireBus4);
-DA_DiscreteOutput T1_XY004 = DA_DiscreteOutput(3, LOW); // V1
-DA_DiscreteOutput N1_XY004 = DA_DiscreteOutput(11, LOW); // V2
-DA_DiscreteOutput N2_XY004 = DA_DiscreteOutput(10, LOW); // V3
-DA_DiscreteOutput N3_XY004 = DA_DiscreteOutput(9, LOW); // V4
-DA_AnalogInput T1_PT004 = DA_AnalogInput(A1, 0.0, 5.); // min max
-DA_AnalogInput N1_PT004 = DA_AnalogInput(A2, 0.0, 5.); // min max
-DA_AnalogInput N2_PT004 = DA_AnalogInput(A6, 0.0, 5.); // min max
-DA_AnalogInput N3_PT004 = DA_AnalogInput(A7, 0.0, 5.); // min max
+DallasTemperature B1N1_T1_TT004(& oneWireBus1);
+DallasTemperature B1N1_N1_TT004(& oneWireBus2);
+DallasTemperature B1N1_N2_TT004(& oneWireBus3);
+DallasTemperature B1N1_N3_TT004(& oneWireBus4);
+DA_DiscreteOutput B1N1_T1_XY004 = DA_DiscreteOutput(3, LOW); // V1
+DA_DiscreteOutput B1N1_N1_XY004 = DA_DiscreteOutput(11, LOW); // V2
+DA_DiscreteOutput B1N1_N2_XY004 = DA_DiscreteOutput(10, LOW); // V3
+DA_DiscreteOutput B1N1_N3_XY004 = DA_DiscreteOutput(9, LOW); // V4
+DA_AnalogInput B1N1_T1_PT004 = DA_AnalogInput(A1, 0.0, 5.); // min max
+DA_AnalogInput B1N1_N1_PT004 = DA_AnalogInput(A2, 0.0, 5.); // min max
+DA_AnalogInput B1N1_N2_PT004 = DA_AnalogInput(A6, 0.0, 5.); // min max
+DA_AnalogInput B1N1_N3_PT004 = DA_AnalogInput(A7, 0.0, 5.); // min max
 
-DA_DiscreteOutput MAX285TX_ENABLE = DA_DiscreteOutput( 6, HIGH );  // 1 is enable, 0 rx enabled
+//DA_DiscreteOutput MAX285TX_ENABLE = DA_DiscreteOutput( 6, HIGH );  // 1 is enable, 0 rx enabled
 
 #ifdef PROCESS_TERMINAL
 DA_DiscreteOutput LED = DA_DiscreteOutput(13, HIGH); // for debugging
@@ -65,6 +65,8 @@ DA_DiscreteOutput LED = DA_DiscreteOutput(13, HIGH); // for debugging
 
 // HEARTBEAT
 unsigned int heartBeat = 0;
+
+
 struct _AlarmEntry
 {
   time_t epoch;
@@ -128,10 +130,10 @@ void init1WireTemperatureSensor(DallasTemperature * sensor, int idx)
 
 void initHydroponicOneWireTemps()
 {
-  init1WireTemperatureSensor(& T1_TT004, 1);
-  init1WireTemperatureSensor(& N1_TT004, 2);
-  init1WireTemperatureSensor(& N2_TT004, 3);
-  init1WireTemperatureSensor(& N3_TT004, 4);
+  init1WireTemperatureSensor(& B1N1_T1_TT004, 1);
+  init1WireTemperatureSensor(& B1N1_N1_TT004, 2);
+  init1WireTemperatureSensor(& B1N1_N2_TT004, 3);
+  init1WireTemperatureSensor(& B1N1_N3_TT004, 4);
 }
 
 int polling; // 1=polling analogs, 2=polling digitals, -1 nothing
@@ -184,23 +186,23 @@ void doPoll1WireTemperature(DallasTemperature * sensor, int idx)
 
 void doProcess1WireTemperatures()
 {
-  doPoll1WireTemperature(& T1_TT004, 0);
-  doPoll1WireTemperature(& N1_TT004, 1);
-  doPoll1WireTemperature(& N2_TT004, 2);
-  doPoll1WireTemperature(& N3_TT004, 3);
+  doPoll1WireTemperature(& B1N1_T1_TT004, 0);
+  doPoll1WireTemperature(& B1N1_N1_TT004, 1);
+  doPoll1WireTemperature(& B1N1_N2_TT004, 2);
+  doPoll1WireTemperature(& B1N1_N3_TT004, 3);
 }
 
 void doReadAnalogs()
 {
-  T1_PT004.refresh();
-  N1_PT004.refresh();
-  N2_PT004.refresh();
+  B1N1_T1_PT004.refresh();
+  B1N1_N1_PT004.refresh();
+  B1N1_N2_PT004.refresh();
 
 #ifdef TRACE_ANALOGS
-  T1_PT004.serialize(tracePort, true);
-  N1_PT004.serialize(tracePort, true);
-  N2_PT004.serialize(tracePort, true);
-  N3_PT004.serialize(tracePort, true);
+  B1N1_T1_PT004.serialize(tracePort, true);
+  B1N1_N1_PT004.serialize(tracePort, true);
+  B1N1_N2_PT004.serialize(tracePort, true);
+  B1N1_N3_PT004.serialize(tracePort, true);
 #endif
 
 }
@@ -216,14 +218,14 @@ void doReadAnalogs()
 void refreshModbusRegisters()
 {
 
-  modbusRegisters[HR_TEMPERATURE1] = T1_TT004.getTempCByIndex(0) * 100;
-  modbusRegisters[HR_TEMPERATURE2] = N1_TT004.getTempCByIndex(0) * 100;
-  modbusRegisters[HR_TEMPERATURE3] = N2_TT004.getTempCByIndex(0) * 100;
-  modbusRegisters[HR_TEMPERATURE4] = N3_TT004.getTempCByIndex(0) * 100;
-  modbusRegisters[HR_PRESSURE1] = T1_PT004.getScaledSample() * 100;
-  modbusRegisters[HR_PRESSURE2] = N1_PT004.getScaledSample() * 100;
-  modbusRegisters[HR_PRESSURE3] = N2_PT004.getScaledSample() * 100;
-  modbusRegisters[HR_PRESSURE4] = N2_PT004.getScaledSample() * 100;
+  modbusRegisters[HR_TEMPERATURE1] = B1N1_T1_TT004.getTempCByIndex(0) * 100;
+  modbusRegisters[HR_TEMPERATURE2] = B1N1_N1_TT004.getTempCByIndex(0) * 100;
+  modbusRegisters[HR_TEMPERATURE3] = B1N1_N2_TT004.getTempCByIndex(0) * 100;
+  modbusRegisters[HR_TEMPERATURE4] = B1N1_N3_TT004.getTempCByIndex(0) * 100;
+  modbusRegisters[HR_PRESSURE1] = B1N1_T1_PT004.getScaledSample() * 100;
+  modbusRegisters[HR_PRESSURE2] = B1N1_N1_PT004.getScaledSample() * 100;
+  modbusRegisters[HR_PRESSURE3] = B1N1_N2_PT004.getScaledSample() * 100;
+  modbusRegisters[HR_PRESSURE4] = B1N1_N3_PT004.getScaledSample() * 100;
   modbusRegisters[HR_HEARTBEAT] = heartBeat;
 }
 
@@ -275,14 +277,14 @@ void checkAndResetDO(unsigned int bitOffset, DA_DiscreteOutput * aDO)
 
 void processValveCommands()
 {
-  checkAndActivateDO(VALVE1_OPEN, & T1_XY004);
-  checkAndResetDO(VALVE1_CLOSE, & T1_XY004);
-  checkAndActivateDO(VALVE2_OPEN, & N1_XY004);
-  checkAndResetDO(VALVE2_CLOSE, & N1_XY004);
-  checkAndActivateDO(VALVE3_OPEN, & N2_XY004);
-  checkAndResetDO(VALVE3_CLOSE, & N2_XY004);
-  checkAndActivateDO(VALVE4_OPEN, & N3_XY004);
-  checkAndResetDO(VALVE4_CLOSE, & N3_XY004);
+  checkAndActivateDO(VALVE1_OPEN, & B1N1_T1_XY004);
+  checkAndResetDO(VALVE1_CLOSE, & B1N1_T1_XY004);
+  checkAndActivateDO(VALVE2_OPEN, & B1N1_N1_XY004);
+  checkAndResetDO(VALVE2_CLOSE, & B1N1_N1_XY004);
+  checkAndActivateDO(VALVE3_OPEN, & B1N1_N2_XY004);
+  checkAndResetDO(VALVE3_CLOSE, & B1N1_N2_XY004);
+  checkAndActivateDO(VALVE4_OPEN, & B1N1_N3_XY004);
+  checkAndResetDO(VALVE4_CLOSE, & B1N1_N3_XY004);
 }
 
 void processModbusCommands()
